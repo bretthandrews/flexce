8b661308760e9368813bb19c6722c9906b60dd90
Brett Andrews <brett.h.andrews@gmail.com>
2016-06-01 12:49:54 -0400

    Update snippet to read in yields

--- examples/read_yields.py
+++ examples/read_yields.py
@@ -3,18 +3,19 @@
 import os
 from os.path import join
 
-from fileio.cfg_io import read_cfg
+path_flexce_root = join(os.path.expanduser('~'), 'flexCE')
+path_flexce = join(path_flexce_root, 'flexCE')
+sys.path.insert(0, path_flexce)
+from fileio.cfg_io import read_sim_cfg
 from utils import define_mass_bins
 from utils import load_yields
 
-home = os.path.expanduser('~')
-path_flexce = join(home, 'flexCE', 'flexCE')
-path_config = join(path_flexce, 'config')
+path_config = join(path_flexce_root, 'config')
 path_data = join(path_flexce, 'data')
 file_in = join(path_config, 'sim0.cfg')
 
 (sim_id, yld_args, initialize_args, mass_bins_args, snia_dtd_args,
- inflows_args, outflows_args, warmgasres_args, sf_args) = read_cfg(file_in)
+ inflows_args, outflows_args, warmgasres_args, sf_args) = read_sim_cfg(file_in)
 
 mass_bins = define_mass_bins(**mass_bins_args)
 ylds = load_yields(path_data, yld_args, mass_bins)
