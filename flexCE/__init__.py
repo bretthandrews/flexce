6d824ef64569fa3200efb2e554e50283954f8ca1
Brett Andrews <brett.h.andrews@gmail.com>
2018-06-04 12:38:04 -0400

    adds version to init

---
+++ flexCE/__init__.py
@@ -0,0 +1,7 @@
+# @Author: Brett Andrews <andrews>
+# @Date:   2018-04-16 20:04:30
+# @Last modified by:   andrews
+# @Last modified time: 2018-06-04 12:06:94
+
+# Set the flexCE version
+__version__ = '1.0.1dev'
