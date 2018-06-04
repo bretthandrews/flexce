805daf7bbd79307a4ecb71a962898a1a09431096
Brett Andrews <brett.h.andrews@gmail.com>
2018-06-04 15:07:32 -0400

    improves setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
-from distutils.core import setup
+import os
 
-NAME = 'flexCE'
+from setuptools import setup, find_packages
+# from distutils.core import setup
+
+NAME = 'flexce'
 # do not use x.x.x-dev.  things complain.  instead use x.x.xdev
 VERSION = '1.0.2'
 # RELEASE = 'dev' not in VERSION
 
-setup(name=NAME,
-      version=VERSION,
-      description='Flexible Galactic Chemical Evolution Model',
-      author='Brett Andrews',
-      author_email='brett.h.andrews@gmail.com',
-      url='https://github.com/bretthandrews/flexCE',
-      license='MIT',
-      packages=['flexCE'])
+# requirements
+requirements_file = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+install_requires = [line.strip().replace('==', '>=') for line in open(requirements_file)
+                    if not line.strip().startswith('#') and line.strip() != '']
+
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=find_packages(),
+    description='Flexible Galactic Chemical Evolution Model',
+    author='Brett Andrews',
+    author_email='brett.h.andrews@gmail.com',
+    url='https://github.com/bretthandrews/flexce',
+    license='MIT',
+    include_package_data=True,
+    install_requires=install_requires,
+)
