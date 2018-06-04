1fe852569b9118c2f7442594b582bf19aafdb7be
Brett Andrews <brett.h.andrews@gmail.com>
2018-06-04 15:07:09 -0400

    imports flexce in init

--- flexce/__init__.py
+++ flexce/__init__.py
@@ -1,7 +1,10 @@
 # @Author: Brett Andrews <andrews>
 # @Date:   2018-04-16 20:04:30
 # @Last modified by:   andrews
-# @Last modified time: 2018-06-04 12:06:41
+# @Last modified time: 2018-06-04 14:06:74
+
+
+from flexce import *
 
 # Set the flexCE version
 __version__ = '1.0.2'
