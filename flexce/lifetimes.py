dd627bc8187a9349e222dbce85be4db09d548537
Brett Andrews <brett.h.andrews@gmail.com>
2018-06-05 12:27:17 -0400

    init import of lifetimes

---
+++ flexce/lifetimes.py
@@ -0,0 +1,82 @@
+# @Author: Brett Andrews <andrews>
+# @Date:   2018-06-05 12:06:85
+# @Last modified by:   andrews
+# @Last modified time: 2018-06-05 12:06:49
+
+"""
+FILE
+    lifetimes.py
+
+DESCRIPTION
+    Utility functions for populating a stellar lifetimes.
+"""
+
+import numpy as np
+
+
+def lifetime_int(mass):
+    """Compute the lifetime of intermediate mass star (M=0.6-6.6 Msun).
+
+    Args:
+        mass (array): Stellar mass.
+
+    Returns:
+        array: Stellar lifetimes.
+    """
+    return 10.**((1.338 - np.sqrt(1.790 - 0.2232 * (7.764 - np.log10(mass)))) /
+                 0.1116 - 9.) * 1000.
+
+
+def lifetime_high(mass):
+    """Compute the lifetime of a high mass star (M > 6.6 Msun).
+
+    Args:
+        mass (array): Stellar mass.
+
+    Returns:
+        array: Stellar lifetimes.
+    """
+    return (1.2 * mass**(-1.85) + 0.003) * 1000.
+
+
+def invert_lifetime(time):
+    """Compute stellar masses given lifetimes (valid for <50 Gyr).
+
+    Args:
+        time (array): Lifetime in Myr.
+
+    Returns:
+        array: Stellar masses.
+    """
+    ind_int = (time >= 40.) & (time < 50000)
+    ind_high = (time > 1.) & (time < 40.)
+
+    mass = np.zeros(len(time))
+    mass[ind_int] = invert_lifetime_int(time[ind_int])
+    mass[ind_high] = invert_lifetime_high(time[ind_high])
+
+    return mass
+
+
+def invert_lifetime_int(time):
+    """Compute stellar masses given lifetimes (for 40 Myr-50 Gyr).
+
+    Args:
+        time (array): Lifetime in Myr.
+
+    Returns:
+        array: Stellar masses.
+    """
+    return 10.**(7.764 - (1.790 - (0.3336 - 0.1116 * np.log10(time / 1000.))**2.) / 0.2232)
+
+
+def invert_lifetime_high(time):
+    """Compute stellar masses given lifetimes (valid for <40 Myr).
+
+    Args:
+        time (array): Lifetime in Myr.
+
+    Returns:
+        array: Stellar masses.
+    """
+    return (((time / 1000.) - 0.003) / 1.2)**(-1. / 1.85)
