c08ced6352b2a80c80c95b6dbadf3217ab94f765
Brett Andrews <brett.h.andrews@gmail.com>
2018-06-05 11:54:08 -0400

    init import of chemevol2

---
+++ flexce/chemevol2.py
@@ -0,0 +1,15 @@
+# @Author: Brett Andrews <andrews>
+# @Date:   2018-06-05 11:06:88
+# @Last modified by:   andrews
+# @Last modified time: 2018-06-05 11:06:14
+
+"""
+FILE
+    chemevol.py
+
+USAGE
+    gal = ChemEvol(**params)
+
+DESCRIPTION
+    Main module for running the chemical evolution model.
+"""
