e46395aab91b02a1dd5bf560af5517ae9ab0633f
Brett Andrews <brett.h.andrews@gmail.com>
2018-06-06 12:31:12 -0400

    minor reformatting of snia_dtd

--- flexce/snia.py
+++ flexce/snia.py
@@ -1,7 +1,7 @@
 # @Author: Brett Andrews <andrews>
 # @Date:   2018-06-06 12:06:40
 # @Last modified by:   andrews
-# @Last modified time: 2018-06-06 12:06:89
+# @Last modified time: 2018-06-06 12:06:18
 
 """
 FILE
@@ -11,6 +11,8 @@ DESCRIPTION
     Functions for computing the SNIa delay time distribution.
 """
 
+import traceback
+
 import flexce.utils
 
 
@@ -18,15 +20,15 @@ def snia_dtd(func='exponential', kwargs=None):
     """Set SNIa delay time distribution.
 
     Args:
-        func (str): functional form of DTD. Defaults to 'exponential'.
+        func (str): functional form of DTD. Default is 'exponential'.
         kwargs (dict): keyword arguments to pass to individual DTD
-            functions. Defaults to None.
+            functions. Default is ``None``.
 
     Returns:
         dict: SNIa params
     """
     kwargs = flexce.utils.none_to_empty_dict(kwargs)
-    self.snia_param = {'func': func, 'k': kwargs}
+    snia_param = {'func': func, 'k': kwargs}
     try:
         if func == 'exponential':
             snia_dtd_exp(**kwargs)
@@ -38,10 +40,12 @@ def snia_dtd(func='exponential', kwargs=None):
             snia_dtd_single_degenerate(**kwargs)
     except TypeError:
         print(traceback.print_exc())
-        print('\nValid keywords:\n')
-        print('exponential: timescale, min_snia_time, snia_fraction\n')
-        print('power_law: min_snia_time, nia_per_mstar, slope\n')
-        print('prompt_delayed: A, B,  min_snia_time\n')
-        print('single_degenerate: no keywords\n')
+        print(
+            '\nValid keywords:\n'
+            'exponential: timescale, min_snia_time, snia_fraction\n'
+            'power_law: min_snia_time, nia_per_mstar, slope\n'
+            'prompt_delayed: A, B,  min_snia_time\n'
+            'single_degenerate: no keywords\n'
+        )
 
     return snia_param
