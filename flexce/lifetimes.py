bd9572dde72a547d6e56bab5529959cb799acb2c
Brett Andrews <brett.h.andrews@gmail.com>
2018-06-06 12:04:33 -0400

    refactors frac_evolve

--- flexce/lifetimes.py
+++ flexce/lifetimes.py
@@ -1,7 +1,7 @@
 # @Author: Brett Andrews <andrews>
 # @Date:   2018-06-05 12:06:85
 # @Last modified by:   andrews
-# @Last modified time: 2018-06-05 12:06:49
+# @Last modified time: 2018-06-06 11:06:23
 
 """
 FILE
@@ -101,3 +101,74 @@ def set_lifetimes(mass_ave):
     return tau
 
 
+def frac_evolve(time, bins, alpha, breaks):
+    """Compute fraction of stars born in a given timestep will evolve.
+
+    Figure out which mass bins will have at least some stars evolving in
+    a given timestep (ind_ev) and what fraction of the stars in that mass
+    bin will evolve (frac_ev).
+
+    Args:
+        time, mass_bins, alpha, mass_breaks
+    Returns:
+        ind_ev, frac_ev, frac_ev_tot
+    """
+    # lowest mass star that would evolve in a timestep
+    m_ev = invert_lifetime(time)
+    m_ev[0] = bins[-1]
+
+    # integrate the IMF in each mass bin
+    n_bins = len(bins) - 1
+    alpha2 = 2 - alpha
+    norm_factor = flexce.imf.normalize_imf(alpha, breaks)
+
+    mass_int_tmp = np.zeros(n_bins)
+    for jj in range(n_bins):
+        mbin = bins[jj:jj + 2]
+        mass_int_tmp[jj] = integrate_multi_power_law(mbin, alpha2, breaks, norm_factor)
+
+    # figure out which mass bins will have at least some stars evolving in
+    # a given timestep (ind_ev) and what fraction of the stars in that mass
+    # bin will evolve (frac_ev)
+    ind_ev = []
+    frac_ev = []
+
+    for ii in range(len(time) - 1):
+        indtmp = []
+        fractmp = []
+
+        for jj in range(n_bins):
+            # mass bin that spans the top end of the mass range that will
+            # evolve in this timestep
+            if (m_ev[ii] < bins[jj + 1]) and (m_ev[ii] >= bins[jj]):
+                mlow_tmp = np.maximum(bins[jj], m_ev[ii + 1])
+                mbin_tmp = np.array([mlow_tmp, m_ev[ii]])
+
+            # mass bins fully contained within the mass range that will
+            # evolve in this timestep
+            elif (bins[jj] < m_ev[ii]) and (bins[jj] > m_ev[ii + 1]):
+                mbin_tmp = bins[jj:jj + 2]
+
+            # mass bin that spans bottom top end of the mass range that
+            # will evolve in this timestep
+            elif (m_ev[ii + 1] < bins[jj + 1]) and (m_ev[ii + 1] > bins[jj]):
+                mbin_tmp = np.array([m_ev[ii + 1], bins[jj + 1]])
+
+            else:
+                continue
+
+            indtmp.append(jj)
+            mass_int2_tmp = integrate_multi_power_law(mbin_tmp, alpha2, breaks, norm_factor)
+            fractmp.append(mass_int2_tmp / mass_int_tmp[jj])
+
+        indtmp = np.array(indtmp)
+        ind_ev.append(indtmp)
+
+        fractmp = np.array(fractmp)[:, 0]
+        frac_ev.append(fractmp)
+
+    frac_ev_tot = np.zeros(n_bins)
+    for kk in range(len(time) - 1):
+        frac_ev_tot[ind_ev[kk]] += frac_ev[kk]
+
+    return ind_ev, frac_ev, frac_ev_tot
