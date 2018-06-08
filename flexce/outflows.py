0b5cff8cf0f799bd6ae75c128de7c36f6cb9df24
Brett Andrews <brett.h.andrews@gmail.com>
2018-06-07 21:36:31 -0400

    adds to docstrings

--- flexce/outflows.py
+++ flexce/outflows.py
@@ -1,7 +1,7 @@
 # @Author: Brett Andrews <andrews>
 # @Date:   2018-06-07 21:06:39
 # @Last modified by:   andrews
-# @Last modified time: 2018-06-07 21:06:90
+# @Last modified time: 2018-06-07 21:06:17
 
 """
 FILE
@@ -36,6 +36,10 @@ def set_outflows(
             galaxy in the outflow (if ``source`` is 'stellar_ejecta').
             Default is 0.15.
 
+    Returns:
+        dict, float, float: Outflow parameters; outflow mass-loading
+            parameter; and fraction of stellar ejecta that leaves in
+            the outflow.
     """
     params = {
         'source': source,
@@ -60,6 +64,22 @@ def set_outflows(
 
 
 def outflow_calc(params, eta, feject, timestep, sfr, stellar_ejecta):
+    """Calculate outflowing mass.
+
+    Args:
+        params (dict): Outflow parameters.
+        eta (float): Outflow mass-loading parameter. Only used if
+            ``source`` is 'ism'.
+        feject (float): Fraction of new stellar ejecta that leaves the
+            galaxy in the outflow (if ``source`` is 'stellar_ejecta').
+        timestep (int): Time step.
+        sfr (float): Star formation rate.
+        stellar_ejecta (float): Mass ejected by CCSN, SNIa, and AGB
+            stars.
+
+    Returns:
+        float: outflowing mass.
+    """
 
     if params['source'] == 'ism':
         if params['variable_eta'] is not None:
