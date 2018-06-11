b41ea8785c6b01668f34425484ad96381d261f87
Brett Andrews <brett.h.andrews@gmail.com>
2018-06-11 13:58:37 -0400

    updates setting SF law

--- flexce/star_formation.py
+++ flexce/star_formation.py
@@ -1,7 +1,7 @@
 # @Author: Brett Andrews <andrews>
 # @Date:   2018-06-11 13:06:00
 # @Last modified by:   andrews
-# @Last modified time: 2018-06-11 13:06:30
+# @Last modified time: 2018-06-11 13:06:77
 
 """
 FILE
@@ -12,22 +12,27 @@ DESCRIPTION
 """
 
 
-def star_formation(self, nu_kslaw=2.5e-10, N_kslaw=1.4):
-    '''From Kennicutt (1998): Sigma_SFR = 2.5e-4 * Sigma_gas^1.4, where
-    Sigma_SFR [=] Msun yr^-1 kpc^-2 and Sigma_gas [=] Msun yr^-1.  nu =
-    2.5-10 if Sigma_SFR is converted into units of Msun yr^-1 pc^-2.  '''
-    self.nu_kslaw = nu_kslaw
-    self.N_kslaw = N_kslaw
-    self.sf_param = dict(nu=nu_kslaw, N=N_kslaw)
+def set_sflaw(nu_kslaw=1e-9, N_kslaw=1.):
+    """Set parameters of the star formation law.
 
+    From Kennicutt (1998):
+        Sigma_SFR = nu * Sigma_gas^N, where
+            Sigma_SFR [=] Msun yr^-1 kpc^-2,
+            Sigma_gas [=] Msun yr^-1,
+            N = 1.4, and
+            nu = 2.5e-4 (= 2.5e-10 if Sigma_SFR is converted into units
+                of Msun yr^-1 pc^-2).
+    Args:
+        nu_kslaw (float): Normalization constant of Kennicutt-Schmidt
+            Law. Default is 1e-9 (for Sigma_SFR in units of
+            Msun yr^-1 pc^-2).
+        N_kslaw (float): Power law slope of Kennicutt-Schmidt Law.
+            Default is 1.
 
-def sf_law(self, mgas):
-    '''Calculate the SFR [Msun/yr] given the gas mass and the two free
-    parameters that determine the SF law: nu_kslaw [Gyr^-1] and N_kslaw
-    (~1.0--2.0).
+    Returns:
+        dict: Star formation law parameters.
+    """
+    params = {'nu': nu_kslaw, 'N': N_kslaw}
 
-    From Kennicutt (1998): Sigma_SFR = 2.5e-4 * Sigma_gas^1.4, where
-    Sigma_SFR [=] Msun yr^-1 kpc^-2 and Sigma_gas [=] Msun yr^-1.  nu =
-    2.5-10 if Sigma_SFR is converted into units of Msun yr^-1 pc^-2.
-    '''
+    return params
     return (self.nu_kslaw) * self.area * (mgas / self.area)**self.N_kslaw
