1e635a26ae9cb14368d10ce8c9d65f5148a94a00
Brett Andrews <brett.h.andrews@gmail.com>
2018-07-13 14:10:10 -0400

    streamlines run_flexce

--- flexce/run_flexce.py
+++ flexce/run_flexce.py
@@ -1,14 +1,14 @@
 # @Author: Brett Andrews <andrews>
 # @Date:   2018-05-30 17:05:89
 # @Last modified by:   andrews
-# @Last modified time: 2018-07-13 13:07:99
+# @Last modified time: 2018-07-13 14:07:46
 
 """
 FILE
     run_flexce
 
 USAGE
-    run_flexce sim-config-file
+    run_flexce config_file --path_out path/to/output
 
     Example::
 
@@ -16,21 +16,14 @@ USAGE
 
 DESCRIPTION
     Run flexCE from the command line using a config file.
-
 """
-
-from __future__ import print_function, division, absolute_import
 
 import os
 from os.path import join
 
 import click
 
-import flexce.abundances
 from flexce.chemevol import ChemEvol
-from flexce.fileio import yml
-import flexce.utils
-from flexce.yields import Yields
 
 
 @click.command()
@@ -39,29 +32,19 @@ from flexce.yields import Yields
 def main(config_file, path_out):
     """
     Args:
-        config_file (str): Config file name.  Default is ``sim0.yml``.
+        config_file (str): Config file name.  Default is ``sim0.yml``
             (fiducial parameters from Andrews et al. 2017).
         path_out (str): Path to output dir.  Default is current dir.
     """
     path_flexce = join(os.path.abspath(os.path.dirname(__file__)), '')
-    path_data = join(path_flexce, 'data')
 
     if config_file is None:
-        print('No config file specified. Using default parameters.')
         config_file = join(path_flexce, 'data', 'config', 'sim0.yml')
 
     if path_out is None:
-        print('No output path specified. Using current working directory.')
         path_out = os.getcwd()
 
-    params = yml.read(config_file)
-
-    mass_bins = flexce.utils.set_mass_bins(**params['mass_bins'])
-
-    params['yld_args'] = flexce.utils.set_yields(params['yields'])
-    ylds = Yields(params=params['yields'], mass_bins=mass_bins, path=path_data)
-
-    gal = ChemEvol(params, ylds)
+    gal = ChemEvol(params=config_file)
     gal.save(path=path_out)
 
 
