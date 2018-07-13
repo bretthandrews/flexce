ec3215ce8aed67ca8789a218c6264911c727cce8
Brett Andrews <brett.h.andrews@gmail.com>
2018-07-13 13:24:10 -0400

    adds function to merge nested dictionaries

--- flexce/tests/test_utils.py
+++ flexce/tests/test_utils.py
@@ -1,12 +1,29 @@
 # @Author: Brett Andrews <andrews>
 # @Date:   2018-04-16 20:04:48
 # @Last modified by:   andrews
-# @Last modified time: 2018-06-14 14:06:94
+# @Last modified time: 2018-07-13 13:07:19
+
+import copy
 
 import numpy as np
 import pytest
 
 import flexce.utils
+
+a1 = {'x': 1}
+a2 = {'x': 2}
+a3 = {'x': {}}
+a4 = {'x': {'y': 1}}
+a5 = {'x': {'y': 2}}
+a6 = {'x': {'y': 1, 'z': 2}}
+a7 = {'x': {'y': 2, 'z': 2}}
+a8 = {'i': 5}
+expected1 = {'x': 1}
+expected2 = {'x': {'y': 1}}
+expected3 = {'x': {'y': 1, 'z': 2}}
+expected4 = {'x': {'y': 2, 'z': 2}}
+expected5 = {'i': 5, 'x': {'y': 2, 'z': 2}}
+expected6 = {'i': 5, 'x': 1}
 
 
 class TestUtils(object):
@@ -20,3 +37,48 @@ class TestUtils(object):
     def test_set_mass_bins(self, params, expected):
         mbins = flexce.utils.set_mass_bins(**params)
         assert mbins == pytest.approx(expected)
+
+
+class TestMerge(object):
+
+    @pytest.mark.parametrize(
+        'aa, bb, expected',
+        [(a8, a7, expected5),
+         (a1, a1, expected1),
+         (a1, a2, expected1),
+         (a4, a4, expected2),
+         (a3, a4, expected2),
+         (a4, a3, expected2),
+         (a4, a5, expected2),
+         (a6, a3, expected3),
+         (a6, a4, expected3),
+         (a6, a5, expected3),
+         (a4, a6, expected3),
+         (a5, a6, expected4),
+         (a7, a8, expected5),
+         (a1, a8, expected6),
+         (a8, a1, expected6),
+         ]
+    )
+    def test_merge_success(self, aa, bb, expected):
+        cc = copy.deepcopy(aa)
+        print('\naa', aa)
+        print('bb', bb)
+        print('expected', expected)
+        assert flexce.utils.merge(cc, bb) == expected
+
+    @pytest.mark.parametrize(
+        'aa, bb',
+        [(a1, a3),
+         (a2, a3),
+         (a3, a1),
+         (a3, a2),
+         (a1, a4),
+         (a2, a4),
+         (a4, a1),
+         (a4, a2),
+         ]
+    )
+    def test_merge_fail(self, aa, bb):
+        with pytest.raises(TypeError):
+            flexce.utils.merge(aa, bb)
--- flexce/utils.py
+++ flexce/utils.py
@@ -1,7 +1,7 @@
 # @Author: Brett Andrews <andrews>
 # @Date:   2018-04-16 20:04:48
 # @Last modified by:   andrews
-# @Last modified time: 2018-06-18 17:06:28
+# @Last modified time: 2018-07-13 13:07:61
 
 """
 FILE
@@ -15,12 +15,9 @@ from __future__ import print_function, division, absolute_import
 
 import os
 from os.path import join
-import sys
 from pathlib import PurePath
 
 import numpy as np
-
-import flexce
 
 
 def set_mass_bins(low=0.1, high=100, dm_low=0.1, dm_high=1., break_mass=8):
@@ -122,3 +119,35 @@ def robust_random_poisson(xx):
                 yy[i] = np.round(item)
 
     return yy
+
+
+def merge(aa, bb, path=None):
+    """Recursively merge two dictionaries (``bb`` into ``aa``).
+
+    Uses ``aa`` as the default if values conflict. Assumes that ``aa``
+    and ``bb`` do not contain lists of dicts.
+
+    Args:
+        aa (dict): Preferred option.
+        bb (dict): Fall back option.
+        path (str): Path down nested dictionaries. Default is ``None``.
+
+    Returns:
+        dict
+    """
+
+    if path is None:
+        path = []
+
+    for key in bb:
+        if key in aa:
+            if isinstance(aa[key], dict) and isinstance(bb[key], dict):
+                merge(aa[key], bb[key], path + [str(key)])
+
+            elif isinstance(aa[key], dict) ^ isinstance(bb[key], dict):  # XOR
+                raise TypeError(f"{'.'.join(path + [str(key)])} must both be dict or non-dict.")
+
+        else:
+            aa[key] = bb[key]
+
+    return aa
