6ac174c72767975a1f79e0673857149796f84365
Brett Andrews <brett.h.andrews@gmail.com>
2018-07-16 11:40:07 -0400

    enables run_flexce to run a multiple simulations

--- flexce/scripts/run_flexce.py
+++ flexce/scripts/run_flexce.py
@@ -1,21 +1,21 @@
 # @Author: Brett Andrews <andrews>
 # @Date:   2018-05-30 17:05:89
 # @Last modified by:   andrews
-# @Last modified time: 2018-07-13 14:07:46
+# @Last modified time: 2018-07-16 11:07:41
 
 """
 FILE
     run_flexce
 
 USAGE
-    run_flexce config_file --path_out path/to/output
+    run_flexce config_files --path_out path/to/output
 
     Example::
 
-        $ run_flexce sim0.yml
+        $ run_flexce sim0.yml --path_out output
 
 DESCRIPTION
-    Run flexCE from the command line using a config file.
+    Run flexCE from the command line using config files.
 """
 
 import os
@@ -28,24 +28,25 @@ from flexce.chemevol import ChemEvol
 
 @click.command()
 @click.option('--path_out', default=None, type=click.Path())
-@click.argument('config_file', default=None, required=False, type=click.Path())
-def main(config_file, path_out):
+@click.argument('config_files', default=None, nargs=-1, required=False, type=click.Path())
+def main(config_files, path_out):
     """
     Args:
-        config_file (str): Config file name.  Default is ``sim0.yml``
+        config_files (str): Config file name.  Default is ``sim0.yml``
             (fiducial parameters from Andrews et al. 2017).
         path_out (str): Path to output dir.  Default is current dir.
     """
     path_flexce = join(os.path.abspath(os.path.dirname(__file__)), '')
 
-    if config_file is None:
-        config_file = join(path_flexce, 'data', 'config', 'sim0.yml')
+    if config_files is None:
+        config_files = [join(path_flexce, 'data', 'config', 'sim0.yml')]
 
     if path_out is None:
         path_out = os.getcwd()
 
-    gal = ChemEvol(params=config_file)
-    gal.save(path=path_out)
+    for cfile in config_files:
+        gal = ChemEvol(params=cfile)
+        gal.save(path=path_out)
 
 
 if __name__ == '__main__':
