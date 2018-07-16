7e2d92957a1527fca63886b3c7edd187c0614d33
Brett Andrews <brett.h.andrews@gmail.com>
2018-07-16 10:28:01 -0400

    enables ``from flexce import ChemEvol``

--- flexce/__init__.py
+++ flexce/__init__.py
@@ -1,10 +1,12 @@
 # @Author: Brett Andrews <andrews>
 # @Date:   2018-04-16 20:04:30
 # @Last modified by:   andrews
-# @Last modified time: 2018-06-04 14:06:74
+# @Last modified time: 2018-07-16 10:07:07
 
 
 from flexce import *
+from .chemevol import ChemEvol
+
 
 # Set the flexCE version
 __version__ = '1.0.2'
